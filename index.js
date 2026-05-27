13e8c11b50a741b1f11b695164f0fce4f8463878
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-05-27 09:54:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomnumber1=Math.floor(Math.random()*6)+1;
+var randomnumber2=Math.floor(Math.random()*6)+1;
+
+var randomdiceimage1="dice"+randomnumber1+".png";
+var randomdiceimage2="dice"+randomnumber2+".png";
+
+var randomimagesource1="images/"+randomdiceimage1;
+var randomimagesource2="images/"+randomdiceimage2;
+
+var image1=document.querySelectorAll("img")[0];
+var image2=document.querySelectorAll("img")[1];
+
+image1.setAttribute("src",randomimagesource1);
+image2.setAttribute("src",randomimagesource2);
+
+if(randomnumber1>randomnumber2){
+  document.querySelector("h1").innerHTML="Player 1 Wins!";
+}
+else if(randomnumber2>randomnumber1){
+  document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
+else{
+  document.querySelector("h1").innerHTML="It's a Draw!";
+}
+
+
